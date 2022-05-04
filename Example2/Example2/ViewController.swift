80d3dd6dcca01baa0e2cc1085ad6574e06f90bcd
Dimitri van Oijen <dimi@xs4all.nl>
2022-05-04 14:37:21 +0200

    Release 1.4.0

---
+++ Example2/Example2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example2
+//
+//  Created by Dimitri van Oijen on 04/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
